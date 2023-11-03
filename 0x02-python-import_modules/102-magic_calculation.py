3c3989ecfa3bb6d3ce18d38f0e1a24c4d7cfac9c
EzzagmounyAZIZ <azizezzagmouny@gmail.com>
2023-11-02 18:07:12 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
+    add = None
+    sub = None
 
     if a < b:
+        if add is None:
+            from magic_calculation_102 import add as magic_add
+            add = magic_add
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
-
+        return c
     else:
-        return(sub(a, b))
+        if sub is None:
+            from magic_calculation_102 import sub as magic_sub
+            sub = magic_sub
+        return sub(a, b)
